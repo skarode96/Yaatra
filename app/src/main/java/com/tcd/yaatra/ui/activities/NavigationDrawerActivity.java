a067b9a237e5ec5f3c9caa500db85b86232548d6
kavi22493@gmail.com <kavi22493@gmail.com>
2020-02-03 10:41:28 +0000

    added navigation activity and updated register activity

---
+++ app/src/main/java/com/tcd/yaatra/ui/activities/NavigationDrawerActivity.java
@@ -0,0 +1,16 @@
+package com.tcd.yaatra.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tcd.yaatra.R;
+
+public class NavigationDrawerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_drawer);
+    }
+}
