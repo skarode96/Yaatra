91c653805f84426795b7481ce87dc36aba16e189
cprasad <cprasad@tcd.ie>
2020-02-27 11:38:36 +0000

    Map cleanup update!

--- app/src/main/java/com/tcd/yaatra/ui/activities/DailyCommuteMapFragment.java
+++ app/src/main/java/com/tcd/yaatra/ui/activities/DailyCommuteMapFragment.java
@@ -224,6 +224,41 @@ public class DailyCommuteMapFragment extends BaseFragment<FragmentDailyCommuteMa
         return true;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+        //unregisterReceiver(MyReceiver);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
 
 }
